b7cca34dd19a5a6fe8fce4ad8b403d80d9d389cf
Ahmad Syangkan Syarip <ahmadssyarif27@gmail.com>
2026-01-01 12:47:10 +0700

    merapikan file di server, memperbaiki bug, dan membuat folder baru untuk belajar http

--- pertemuan5/Project-Mahasantri-PPW./Project-Mahasantri-PPW/server/index.js
+++ pertemuan5/Project-Mahasantri-PPW./Project-Mahasantri-PPW/server/index.js
@@ -1,6 +1,7 @@
 import http from "http"
 import fs from "fs";
 
+const PORT = 3000
 const server = http.createServer( (req, res) => {
     console.log(res.statusCode);
     console.log(req.url);
@@ -9,19 +10,15 @@ const server = http.createServer( (req, res) => {
     res.setHeader("Content-Type", "application/json")
     if (req.url === "/data" && req.method === "GET") {
         const read = fs.readFileSync("../app/module/data.json", "utf-8");
-        
         res.statusCode = 200;
         // res.end()
         res.end(read)
     } else {
         res.statusCode = 404;
         res.end("Page Not Found")
-
     }
     
 });
-
-server.listen(3000, () => {
-    console.log("Server berjalan");
-    
+server.listen(PORT, () => {
+    console.log(`Server was running on port ${PORT}`);
 })
---
+++ pertemuan5/learn-about-server/server/index.js
@@ -0,0 +1,2 @@
+import http from "http";
+import fs from "fs";
